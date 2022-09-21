457f578d339e45af3668b4edebfaf477c0cd837e
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-09-21 11:17:28 +0530

    Number of occurrence
    Problem link to try it yourself : https://practice.geeksforgeeks.org/problems/number-of-occurrence2259/0

---
+++ Basic/Number of occurrence.java
@@ -0,0 +1,23 @@
+/*
+Given a sorted array Arr of size N and a number X, you need to find the number of occurrences of X in Arr.
+
+Example 1:
+
+Input:
+N = 7, X = 2
+Arr[] = {1, 1, 2, 2, 2, 2, 3}
+Output: 4
+Explanation: 2 occurs 4 times in the
+given array.*/
+
+class Solution {
+    int count(int[] arr, int n, int x) {
+        int ans = 0;
+        for(int i=0;i<n;i++)
+        {
+            if(arr[i] == x)ans++;
+        }
+        return ans;
+        // code here
+    }
+}
