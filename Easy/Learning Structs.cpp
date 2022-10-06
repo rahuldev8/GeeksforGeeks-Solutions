9af56f4c7db1c7d4618540c9c0a7aa2062a3db31
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-06 21:10:09 +0530

    Learning Structs
    Problem link to try it yourself : https://practice.geeksforgeeks.org/problems/learning-structs/0

---
+++ Easy/Learning Structs.cpp
@@ -0,0 +1,12 @@
+/*
+Structures are very useful when we want to create Linked Lists and Trees. Here we will learn to create a Linked List struct.*/
+
+
+struct Node
+{
+    //Your code here
+    //Linked list has data and next members
+    int data;
+    Node* next;
+    
+};
