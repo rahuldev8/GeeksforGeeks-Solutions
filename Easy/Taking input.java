fce8180adb70b25febc95938a2c171a4418a7e64
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-06 20:59:51 +0530

    Taking input
    Problem Link to try it Yourself : https://practice.geeksforgeeks.org/problems/taking-input/0

---
+++ Easy/Taking input.java
@@ -0,0 +1,12 @@
+/*
+Before implementing any algorithm, we should be thorough with taking inputs. Here, we will learn how to read inputs.
+You are given two inputs: a(integer), and b(string). You need to take the input and print a and b separated by a space.*/
+
+ void inputData()
+{
+       int a;
+       string b;
+       cin>>a>>b;
+       cout<<a<<" "<<b<<endl;
+      //Use cin to take input and cout a and b with a space between them. Also use an endl after output
+}
