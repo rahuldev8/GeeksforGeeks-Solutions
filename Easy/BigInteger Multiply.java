90f83f0fc22feb15a91c16f41c647a6964c6ee66
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-06 21:12:35 +0530

    BigInteger Multiply
    Problem Link to try it yourself : https://practice.geeksforgeeks.org/problems/biginteger-multiply/0

---
+++ Easy/BigInteger Multiply.java
@@ -0,0 +1,13 @@
+/*
+Given two BigIntegers X and Y. You have to multiply X and Y.*/
+
+class MathematicalOperation{
+    
+    static BigInteger mul(BigInteger x, BigInteger y){
+    
+        // Your code here
+        return x.multiply(y);
+             
+    }
+    
+}
