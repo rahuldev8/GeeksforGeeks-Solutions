0903923d9be9510e2897715b68a4fc9670804ba2
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-06 21:11:38 +0530

    Sum of Big Integers
    Problem link to try it yourself : https://practice.geeksforgeeks.org/problems/sum-of-big-integers/0

---
+++ Easy/Sum of Big Integers.java
@@ -0,0 +1,14 @@
+/*
+Given two positive integers X and Y. You have to add two integers and output their sum.*/
+
+class MathematicalOperation{
+    
+    static BigInteger add(BigInteger x, BigInteger y){
+        
+       return x.add(y);
+            
+        // Your code here
+        
+    }
+    
+}
