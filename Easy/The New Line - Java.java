5695e41cf94fa23aba0e1658d139a6b8edffb460
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-06 20:58:27 +0530

    The New Line - Java
    Problem Link to try it Yourself : https://practice.geeksforgeeks.org/problems/the-new-line-java/0

---
+++ Easy/The New Line - Java.java
@@ -0,0 +1,19 @@
+/*You are familiar with producing output using JAVA In this task,
+you'll be required to write three messages in three separate lines. Write Geeks for Geeks. 
+But wait a minute!! You need to write each word of it in a separate line.
+In the function printIndividualLine(), output each word of Geeks for Geeks in a separate line.
+*/
+
+class Geeks{
+    
+    // Function to print each word in single line
+    static void printIndividualLine(){
+        
+        // Your code here
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+        
+    }
+    
+}
