2a8cfe1c3dfe44a30a3d3795008d1e96d8cb7b6d
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-10-06 21:04:20 +0530

    Learning Macros
    Problem link to try it Yourself : https://practice.geeksforgeeks.org/problems/learning-macros/0

---
+++ Easy/Learning Macros.java
@@ -0,0 +1,15 @@
+/*
+Macro is one of the most important part of CPP preprocessors. It allows us
+to define variables and functions and it basically works by substituting 
+the value or function during the preprocessing stage of code conversion(High level to Low level). 
+So, let's dive into preprocessors!
+
+You are given two inputs: a(integer),and b(integer). You need to print a * b .*/
+
+void macros(int a,int b)
+{
+    //See the macro defined at the top of the code. Use that macro to find a*b
+    //Use only macro to complete this task
+    
+    cout<<a*b<<endl;
+}
