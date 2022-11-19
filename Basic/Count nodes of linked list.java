f2f7ca316d93106970f1858ab7dde74b2b56cfe3
Dev-jr-8 <109261169+Dev-jr-8@users.noreply.github.com>
2022-11-19 20:40:57 +0530

    Count Nodes in Linked List
    Updated in Java, Actual Problem Code -> https://practice.geeksforgeeks.org/problems/count-nodes-of-linked-list/0

--- Basic/Count nodes of linked list.java
+++ Basic/Count nodes of linked list.java
@@ -8,18 +8,22 @@ LinkedList: 1->2->3->4->5
 Output: 5
 Explanation: Count of nodes in the 
 linked list is 5, which is its length.*/
-
-int getCount(struct Node* head)
+class Solution
+{
+    //Function to count nodes of a linked list.
+    public static int getCount(Node head)
     {
-        int count=0;
-        struct Node* th = head;
-        while(th!=NULL)
+        
+        //Code here
+        int count = 0;
+        while(head!=null)
         {
-            th=th->next;
             count++;
+            head = head.next;
         }
         return count;
     }
+    //Time Complexity : o(n)
+    //Space Complexity : o(1)
+}
 
-//Time Complexity: o(n)
-//Space Complexity : o(1)
